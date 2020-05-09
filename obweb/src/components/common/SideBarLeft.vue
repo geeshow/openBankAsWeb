38970e3eee65d4f7349f99b649ca63b87c6c06de
kyutaepark <ken2484@gmail.com>
2020-05-09 23:36:58 +0900

    vue 환경으로 변경 작업

---
+++ obweb/src/components/common/SideBarLeft.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-navigation-drawer
+          v-model="drawer"
+          app
+          left
+  >
+    <v-list dense>
+      <v-list-item link>
+        <v-list-item-action>
+          <v-icon>mdi-folder</v-icon>
+        </v-list-item-action>
+
+        <v-list-item-content>
+          <v-list-item-title>Home</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-list-item link>
+        <v-list-item-action>
+          <v-icon>mdi-home</v-icon>
+        </v-list-item-action>
+
+        <v-list-item-content>
+          <v-list-item-title>Contact</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+
+</style>
